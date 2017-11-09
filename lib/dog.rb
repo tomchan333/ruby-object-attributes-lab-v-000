31f3a7df874e9cd346ba2aac1c10021de06bc130
Tom Chan <tomchanthree@gmail.com>
2017-11-09 04:15:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,5 +14,22 @@ class Dog
   def breed=(new_breed)
     @breed = new_breed
   end
+end
 
+class Person
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
 end
