f94e7aac8dd52353bc9158993b69ef150bbf5d86
Tom Chan <tomchanthree@gmail.com>
2017-11-09 04:14:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
   def name
-    @name 
+    @name
   end
 
   def name=(new_name)
     @name = new_name
   end
 
-  def breed(breed)
-    @breed = breed
+  def breed
+    @breed
   end
 
   def breed=(new_breed)
